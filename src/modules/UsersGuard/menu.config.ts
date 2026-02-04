462d9661a39933870653d83c8ba6f7290771c321
Mounkaila <mounkaila144@gmail.com>
2026-02-04 09:10:14 +0100

    evalution

--- src/modules/UsersGuard/menu.config.ts
+++ src/modules/UsersGuard/menu.config.ts
@@ -5,7 +5,7 @@ export const usersGuardMenuConfig: ModuleMenuConfig = {
   menus: [
     {
       id: 'users',
-      label: 'users3',
+      label: 'users4',
       route: '/admin/users',
       icon: {
         type: 'emoji',
